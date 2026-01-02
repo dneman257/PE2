16633a0fcd83fc83cb56f119cb0f890696e9dd93
dneman257 <daniel.neman@gmail.com>
2026-01-02 15:58:48 -0600

    Updated README.md

---
+++ PE2.py
@@ -0,0 +1,10 @@
+def main():
+    a, b, sum = 1, 2, 0
+    while a <= 4000000:
+        if a % 2 == 0:
+            sum += a
+        a, b = b, a + b
+    return sum
+
+if __name__ == "__main__":
+    print(main())
